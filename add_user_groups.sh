3bdd485571c19a8e3ea165b63512635d232d148e
roomul <mr.roomul@gmail.com>
2021-04-22 16:07:38 +0000

    Add add_user_groups.sh: kiss

---
+++ add_user_groups.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -e
+
+USER="user" # test user
+GROUPS="wheel daemon floppy users audio video adm disk network storage optical scanner lp cdrom tty tape input kvm xbuilder"
+
+for grp in $GROUPS; do
+	printf " :: Add $USER -> $grp \n"
+	addgroup $USER $grp
+done
+
+exit 0
